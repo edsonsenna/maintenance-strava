a511119b2affa0220e9db927e8dc988b18616116
edsondesennajunior <edsonsjr62@gmail.com>
2020-04-27 22:14:21 -0300

    Setting up Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularMaterialModule } from './core/modules/angular-material.module';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/modules/angular-material.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button'; 
+
+const declarations = [
+    
+];
+
+const exports = [
+
+];
+
+const imports = [
+    MatButtonModule
+];
+
+
+@NgModule({
+    declarations: [],
+    exports: [],
+    imports: [...imports]
+})
+export class AngularMaterialModule {}
