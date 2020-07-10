2326b20fabc5e564e64ba0f755b87c2110a1082a
edsondesennajunior <edsonsjr62@gmail.com>
2020-07-10 12:35:10 -0300

    Removing commented lines

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,21 +3,3 @@ require('dotenv').config();
 export const environment = {
   production: true,
 };
-
-// firebaseConfig: {
-//   "apiKey": process.env.FIREBASE_API_KEY,
-//   "authDomain": process.env.FIREBASE_AUTH_DOMAIN,
-//   "databaseURL": process.env.FIREBASE_DB_URL,
-//   "projectId": process.env.FIREBASE_PROJECT_ID,
-//   "storageBucket": process.env.FIREBASE_STORAGE_BUCKET,
-//   "messagingSenderId": process.env.FIREBASE_MESSAGE_SEND_ID,
-//   "appId": process.env.FIREBASE_APP_ID
-// },
-// stravaConfig: {
-//   "stravaAuthUrl": process.env.STRAVA_AUTH_URL,
-//   "stravaBaseApiUrl": process.env.STRAVA_BASE_API_URL,
-//   "clientId": process.env.STRAVA_CLIENT_ID,
-//   "clientSecret": process.env.STRAVA_CLIENT_SECRET,
-//   "grantType": "grant_type=authorization_code",
-//   "grantRefreshType": "grant_type=refresh_token"
-// }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,21 +1,3 @@
-
-    export const environment = {
-        production: false,
-        firebaseConfig: {
-            "apiKey": "AIzaSyA1SaisrSiQ8NSYCE1LABJTT_Zxf43Cq60",
-            "authDomain": "maintance-strava.firebaseapp.com",
-            "databaseURL": "https://maintance-strava.firebaseio.com",
-            "projectId": "maintance-strava",
-            "storageBucket": "maintance-strava.appspot.com",
-            "messagingSenderId": "878034168275",
-            "appId": "1:878034168275:web:22ef570d2ec9fb1cf26726"
-        },
-        stravaConfig: {
-            "stravaAuthUrl": "http://www.strava.com/oauth/",
-            "stravaBaseApiUrl": "https://www.strava.com/api/v3",
-            "clientId": "33524",
-            "clientSecret": "4417fb89842153873e3a17c8c474b39454ecd272",
-            "grantType": "grant_type=authorization_code",
-            "grantRefreshType": "grant_type=refresh_token"
-        }
-    }
+export const environment = {
+  production: false,
+};
