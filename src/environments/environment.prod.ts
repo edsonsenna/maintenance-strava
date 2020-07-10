5adbd30b01becf715a6d919186fd4470babf7b1c
edsondesennajunior <edsonsjr62@gmail.com>
2020-07-10 11:28:06 -0300

    Setting up firebase config with dotenv strategy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    "apiKey": process.env.FIREBASE_API_KEY,
+    "authDomain": process.env.FIREBASE_AUTH_DOMAIN,
+    "databaseURL": process.env.FIREBASE_DB_URL,
+    "projectId": process.env.FIREBASE_PROJECT_ID,
+    "storageBucket": process.env.FIREBASE_STORAGE_BUCKET,
+    "messagingSenderId": process.env.FIREBASE_MESSAGE_SEND_ID,
+    "appId": process.env.FIREBASE_APP_ID
+  },
+  stravaConfig: {}
 };
