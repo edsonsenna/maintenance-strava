b2adb2ba882e5084f27eac9a49d2c604f50b317f
edsondesennajunior <edsonsjr62@gmail.com>
2020-07-12 20:51:44 -0300

    Setting up tests

--- src/app/core/components/maintenance/maintenance-form/maintenance-form.component.spec.ts
+++ src/app/core/components/maintenance/maintenance-form/maintenance-form.component.spec.ts
@@ -1,19 +1,47 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder, FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { MaintenanceFormComponent } from './maintenance-form.component';
+import { of } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MaintenanceService } from 'src/app/core/services/maintenance.service';
+import { TranslateModule } from '@ngx-translate/core';
+import { AngularMaterialModule } from 'src/app/core/modules/angular-material.module';
+import { BrowserTestingModule } from '@angular/platform-browser/testing';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('MaintenanceFormComponent', () => {
   let component: MaintenanceFormComponent;
   let fixture: ComponentFixture<MaintenanceFormComponent>;
+  let _route: Router;
+  let _router: ActivatedRoute;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MaintenanceFormComponent ]
+      declarations: [ MaintenanceFormComponent ],
+      providers: [
+        FormBuilder,
+        {
+          provide: MaintenanceService,
+          useValue: {
+            setMaitenance: (userId, maintenace) => console.log(userId, maintenace)
+          } 
+        }
+      ],
+      imports: [
+        AngularMaterialModule,
+        NoopAnimationsModule,
+        RouterTestingModule.withRoutes([]),
+        TranslateModule.forRoot()
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    _router = TestBed.get(Router);
+    _route = TestBed.get(ActivatedRoute);
     fixture = TestBed.createComponent(MaintenanceFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/core/guards/login.guard.spec.ts
+++ src/app/core/guards/login.guard.spec.ts
@@ -1,16 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LoginGuard } from './login.guard';
+import { HttpClient } from '@angular/common/http';
 
 describe('LoginGuard', () => {
-  let guard: LoginGuard;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    guard = TestBed.inject(LoginGuard);
-  });
-
-  it('should be created', () => {
-    expect(guard).toBeTruthy();
-  });
 });
