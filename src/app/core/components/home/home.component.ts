de105b4368e784399352300f4b59a9817da065e0
edsondesennajunior <edsonsjr62@gmail.com>
2021-03-06 17:05:17 -0300

    Creating box to display message

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import { ActivatedRoute, NavigationStart, NavigationEnd, Router, RouterEvent } from '@angular/router';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { filter, tap } from 'rxjs/operators';
-import { CookieService } from 'ngx-cookie-service';
+import {
+  NavigationStart,
+  NavigationEnd,
+  Router,
+  RouterEvent,
+} from '@angular/router';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { Subscription } from 'rxjs';
 
-import { StravaService } from '../../services/strava.service';
-import { TokenResponse } from '../../interfaces/token-response';
-import { Athlete } from '../../interfaces/athlete';
-import { User } from '../../interfaces/user';
-import { UserService } from '../../services/user.service';
-import { TokenService } from '../../services/token.service';
-import { Collections } from '../../enums/collections';
+import { StravaService } from '@services/strava.service';
+import { Athlete } from '@interfaces/athlete';
+import { User } from '@interfaces/user';
+import { UserService } from '@services/user.service';
+import { TokenService } from '@services/token.service';
+import routes from '@routes/routes';
 
 const AUTH_TOKEN = 'ms-auth-token';
 
@@ -20,28 +21,29 @@ const AUTH_TOKEN = 'ms-auth-token';
   selector: 'ms-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  providers: [StravaService, UserService]
+  providers: [StravaService, UserService],
 })
 export class HomeComponent implements OnInit {
-
   private userCode: string = null;
   private athlete: Athlete = null;
-  private user: User = null;
 
+  public user: User = null;
   public isLoader: boolean = false;
-  public mobileQuery: MediaQueryList; 
+  public mobileQuery: MediaQueryList;
   public title = 'maintenance-strava';
   public menuOptions = [
     {
-      path: '/maintenance',
-      name: 'maintenances'
+      path: routes.MAINTENANCE_LIST,
+      name: 'maintenances',
     },
     {
-      path: '/my-info',
-      name: 'user'
-    }
-  ]
-  
+      path: routes.USER_INFO,
+      name: 'user',
+    },
+  ];
+  public userInfoRoute: String = routes.USER_INFO;
+  public isUserInfoRouteActive: Boolean = false;
+
   private _mobileQueryListener: () => void;
   private _routerSubscription: Subscription = null;
 
@@ -55,31 +57,47 @@ export class HomeComponent implements OnInit {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
-   }
+  }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     this.routerEvents();
+    await this.receiveUserInfo();
   }
 
   ngOnDestroy(): void {
     this.mobileQuery.removeListener(this._mobileQueryListener);
-    this._routerSubscription &&
-      this._routerSubscription.unsubscribe();
+    this._routerSubscription && this._routerSubscription.unsubscribe();
   }
 
   routerEvents() {
-    this._routerSubscription = this._router.events.subscribe((event: RouterEvent) => {
-      if(event instanceof NavigationStart) {
-        this.isLoader = true;
-      } else if(event instanceof NavigationEnd) {
-        this.isLoader = false;
+    this.isUserInfoRouteActive = this._router.isActive(routes.USER_INFO, true);
+    this._routerSubscription = this._router.events.subscribe(
+      (event: RouterEvent) => {
+        if (event instanceof NavigationStart) {
+          this.isLoader = true;
+        } else if (event instanceof NavigationEnd) {
+          this.isLoader = false;
+          this.receiveUserInfo();
+          this.isUserInfoRouteActive = this._router.isActive(routes.USER_INFO, true);
+        }
       }
-    });
+    );
+  }
+
+  async receiveUserInfo() {
+    const userInfo: User = await this._userService
+      .findUserById(this._tokenService.userId)
+      .then((user) => user)
+      .catch(() => null);
+    this.user = {
+      ...this.user,
+      ...userInfo,
+    };
   }
 
   exit() {
     this._tokenService.clearAllTokens();
-    const urlTree = this._router.createUrlTree(['login']);
+    const urlTree = this._router.createUrlTree([routes.LOGIN]);
     this._router.navigateByUrl(urlTree);
   }
 
@@ -87,4 +105,7 @@ export class HomeComponent implements OnInit {
     return this._tokenService.userFirstName;
   }
 
+  get shouldDisplayEmailNotFoundContainer() {
+    return !this.isUserInfoRouteActive && this.user && !this.user?.email;
+  }
 }
---
+++ src/app/core/routes/routes.ts
@@ -0,0 +1,8 @@
+export default {
+    LOGIN: 'login',
+    MAINTENANCE_CREATE: 'maintenance/create',
+    MAINTENANCE_EDIT: 'maintenance/create/:id',
+    MAINTENANCE_LIST: 'maintenance',
+    ROOT: '',
+    USER_INFO: 'my-info'
+};
